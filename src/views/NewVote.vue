98747f75df421a3df23060d9587cb027f687e1ad
hou-teki <houteki@jhcc.co.jp>
2025-08-19 15:42:44 +0900

    Update NewVote Form

--- src/views/NewVote.vue
+++ src/views/NewVote.vue
@@ -1,6 +1,6 @@
 <script setup lang="ts">
-import { reactive, ref } from 'vue'
-import { ElMessage, type FormInstance } from 'element-plus'
+import { reactive } from 'vue'
+import { ElMessage } from 'element-plus'
 import type { VoteForm } from '@/types/vote'
 import { createVote } from '@/apis/votes'
 import { useUserStore } from '@/stores/userStore'
@@ -22,17 +22,6 @@ const form = reactive({
     options: [newOption(), newOption()],
 })
 
-const formRef = ref<FormInstance>()
-
-function addOption() {
-    form.options.push(newOption())
-}
-
-function removeOption(index: number) {
-    if (form.options.length <= 2) return // at least 2 options required
-    form.options.splice(index, 1)
-}
-
 const onSubmit = async () => {
     if (!userStore.isLogin || !userStore.id) {
         ElMessage.error('User not logged in')
@@ -43,23 +32,44 @@ const onSubmit = async () => {
         const formWithCreatorId: VoteForm = { ...form, creatorId: userStore.id }
         await createVote(formWithCreatorId)
         ElMessage.success('Vote created successfully')
+        clearForm()
     } catch (error) {
         ElMessage.error('Failed to create vote')
     }
+}
+
+function addOption() {
+    form.options.push(newOption())
+}
+
+function removeOption(index: number) {
+    if (form.options.length <= 2) return // at least 2 options required
+    form.options.splice(index, 1)
+}
+
+function clearForm() {
+    form.title = ''
+    form.description = ''
+    form.startDate = ''
+    form.endDate = ''
+    form.options = [newOption(), newOption()]
 }
 </script>
 
 <template>
     <el-card>
         <el-form :model="form" label-width="100px">
+            <!-- title -->
             <el-form-item label="Vote title">
                 <el-input v-model="form.title" />
             </el-form-item>
 
+            <!-- description -->
             <el-form-item label="Description">
                 <el-input v-model="form.description" type="textarea" />
             </el-form-item>
 
+            <!-- start date and end date -->
             <el-form-item label="Activity time">
                 <el-col :span="6">
                     <el-date-picker v-model="form.startDate" type="date" placeholder="Start date" style="width: 100%" />
@@ -70,23 +80,28 @@ const onSubmit = async () => {
                 </el-col>
             </el-form-item>
 
+            <!-- Options -->
             <el-form-item label="Options">
-                <div v-for="(opt, idx) in form.options" :key="idx">
-                    <el-form-item :prop="`options.${idx}.label`">
-                        <el-input v-model="opt.label" :placeholder="`Option ${idx + 1}`" clearable />
-                    </el-form-item>
+                <el-space direction="vertical" alignment="start">
+                    <div v-for="(opt, idx) in form.options" :key="idx">
+                        <el-form-item :prop="`options.${idx}.label`">
+                            <el-input v-model="opt.label" :placeholder="`Option ${idx + 1}`" clearable />
+                        </el-form-item>
 
-                    <el-button type="danger" @click="removeOption(idx)"
-                        :disabled="form.options.length <= 2">Delete</el-button>
-                </div>
+                        <el-button type="danger" @click="removeOption(idx)"
+                            :disabled="form.options.length <= 2">Delete</el-button>
+                    </div>
 
-                <el-button type="primary" @click="addOption">Add Option</el-button>
-            </el-form-item>
-
-            <el-form-item>
-                <el-button type="primary" @click="onSubmit">Create</el-button>
-                <el-button>Clear</el-button>
+                    <el-button type="success" @click="addOption">Add Option</el-button>
+                </el-space>
             </el-form-item>
         </el-form>
+
+        <!-- confirm button -->
+        <template #footer>
+            <el-button type="primary" @click="onSubmit">Create</el-button>
+            <el-button @click="clearForm">Clear</el-button>
+        </template>
+
     </el-card>
 </template>
